7d25845b45bc43591e9297a9e48a3f98bcb76ba4
Armans07 <sikder89098@gmail.com>
2023-07-24 22:28:41 +0600

    last commit

--- src/Home/CollegeCard.jsx
+++ src/Home/CollegeCard.jsx
@@ -5,23 +5,24 @@ const CollegeCard = ({ item }) => {
   const { id, image, name, admissionDates, events, researchHistory, sports } = item;
 
   return (
-   <div className='flex'>
-     <div className="card w-96 max-w-md bg-white shadow-md rounded-lg overflow-hidden  card-compact">
-      <img src={image} alt="College" className="w-full h-40 object-cover" />
-      <div className="p-4">
-        <h2 className="text-xl font-semibold mb-2">{name}</h2>
-        <p className="text-gray-600 mb-4">Admission: {admissionDates}</p>
-        <p className="text-sm text-gray-700">Event: {events.join(', ')}</p>
-        <p className="text-sm text-gray-700">History: {researchHistory}</p>
-        <p className="text-sm text-gray-700">Sports: {sports.join(', ')}</p>
+     <div className='flex'>
+       <div className="card w-96 max-w-md bg-white shadow-md rounded-lg overflow-hidden card-compact">
+        <img src={image} alt="College" className="w-full h-50 object-cover" />
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-2">{name}</h2>
+          <p className="text-gray-600 mb-4">Admission: {admissionDates}</p>
+          <p className="text-sm text-gray-700">Event: {events.join(', ')}</p>
+          <p className="text-sm text-gray-700">History: {researchHistory}</p>
+          <p className="text-sm text-gray-700">Sports: {sports.join(', ')}</p>
+        </div>
+        <div className='p-2'>
+        <Link to={`details/${id}`}><button className="p-2 btn btn-outline btn-primary">Details</button></Link>
+        </div>
       </div>
-      <div className='p-2'>
-      <Link to={`details/${id}`}><button className="p-2 btn btn-outline btn-primary">Details</button></Link>
-      </div>
-    </div>
-   </div>
+     </div>
+
+
    
-  
   );
 };
 
